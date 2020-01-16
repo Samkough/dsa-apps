5ad73a9321537ca387f0bc35abae75bad210da7a
Sammy Samkough <ssamkough@DCS-CORP.COM>
2020-01-16 17:27:37 -0500

    Added brute force solution for Decompress Run-Length Encoded List

---
+++ questions/DecompressRunLengthEncodedList.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+
+public class DecompressRunLengthEncodedList {
+    public static int[] bfDecompressList(int[] nums) {
+        int length = 0;
+        for (int i = 0; i < nums.length; i+=2) {
+            length += nums[i];
+        }
+        
+        int[] output = new int[length];
+        int temp = 0;
+        for (int i = 0; i < nums.length; i+=2) {
+            int freq = nums[i];
+            int val = nums[i+1];
+            int stop = temp + freq;
+            for (int j = temp; j < stop; j++) {
+                output[j] = val;
+            }
+            temp += freq;
+        }
+        
+        return output;
+    }
+
+    public static void main (String args[]) {
+        int[] nums1 = new int[]{1,2,3,4};
+        System.out.println(Arrays.toString(nums1) + ": " + Arrays.toString(bfDecompressList(nums1)));
+
+        int[] nums2 = new int[]{1,2,3,4,5,6};
+        System.out.println(Arrays.toString(nums2) + ": " + Arrays.toString(bfDecompressList(nums2)));
+
+        int[] nums3 = new int[]{55,11,70,26,62,64};
+        System.out.println(Arrays.toString(nums3) + ": " + Arrays.toString(bfDecompressList(nums3)));
+
+        int[] nums4 = new int[]{1,4,1,4,1,4,1,4};
+        System.out.println(Arrays.toString(nums4) + ": " + Arrays.toString(bfDecompressList(nums4)));
+    }
+}
