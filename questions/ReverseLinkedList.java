85f1bae9bc22ce08aa4e3c6d77c87ef7d6bd60b9
Sammy Samkough <ssamkough@DCS-CORP.COM>
2020-01-28 18:47:51 -0500

    Added reverse linked list question and iterative solution

---
+++ questions/ReverseLinkedList.java
@@ -0,0 +1,51 @@
+public class ReverseLinkedList {
+    public static ListNode head;
+
+    public static class ListNode {
+        int data;
+        ListNode next;
+    
+        public ListNode(int x) { 
+            data = x;
+        }
+    }
+
+    // O(n)
+    public static ListNode reverseLinkedListIteratively(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        ListNode next = null;
+
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+
+    public static void printList(ListNode node) 
+    { 
+        while (node != null) { 
+            System.out.print(node.data + " "); 
+            node = node.next; 
+        } 
+    }
+
+    public static void main (String args[]) {
+        ReverseLinkedList.head = new ListNode(85); 
+        ReverseLinkedList.head.next = new ListNode(15); 
+        ReverseLinkedList.head.next.next = new ListNode(4); 
+        ReverseLinkedList.head.next.next.next = new ListNode(20); 
+  
+        System.out.println("Given Linked List:"); 
+        ReverseLinkedList.printList(head); 
+
+        head = ReverseLinkedList.reverseLinkedListIteratively(head); 
+
+        System.out.println("\nReversed Linked List:"); 
+        ReverseLinkedList.printList(head); 
+    }
+}
