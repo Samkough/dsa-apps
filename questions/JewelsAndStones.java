92797c35ec22a643598e5fd965ae99adf2187479
Sammy Samkough <sammysamkough@gmail.com>
2020-07-02 17:22:33 -0400

    Added another solution using HashMaps

--- questions/JewelsAndStones.java
+++ questions/JewelsAndStones.java
@@ -1,5 +1,7 @@
-import java.util.HashSet;
 import java.util.Set;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.HashMap;
 
 /*
 The letters in J are guaranteed distinct,
@@ -9,7 +11,7 @@ a different type of stone from "A".
 */
 public class JewelsAndStones {
     // O(n^2)
-    public static int bfNumStonesAreJewels (String J, String S) {
+    public static int bfNumStonesAreJewels(String J, String S) {
         int numOfJewels = 0;
 
         for (int i = 0; i < S.length(); i++) {
@@ -24,31 +26,56 @@ public class JewelsAndStones {
         return numOfJewels;
     }
 
-    // O(n)
-    public static int eNumStonesAreJewels (String J, String S) {
+    // O(n) w/ HashSet
+    public static int eNumStonesAreJewels(String J, String S) {
         Set<Character> uniqueStones = new HashSet<Character>();
-        
+
         for (int i = 0; i < J.length(); i++) {
             uniqueStones.add(J.charAt(i));
         }
-        
+
         int numJewels = 0;
         for (int i = 0; i < S.length(); i++) {
             if (uniqueStones.contains(S.charAt(i))) {
                 numJewels++;
             }
         }
-        
+
         return numJewels;
     }
 
-    public static void main (String args[]) {
+    // O(n) w/ HashMap
+    public int e2numJewelsInStones(String J, String S) {
+        Map<Character, Integer> map = new HashMap<Character, Integer>();
+
+        for (int i = 0; i < J.length(); i++) {
+            map.put(J.charAt(i), i);
+        }
+
+        int count = 0;
+        for (int i = 0; i < S.length(); i++) {
+            if (map.containsKey(S.charAt(i))) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String args[]) {
         String j1 = "ab";
         String s1 = "aAAbbbb";
-        System.out.println("Jewels: " + j1 + "\nStones: " + s1 + "\nNumber of Stones that are Jewels: " + bfNumStonesAreJewels(j1, s1));
+        System.out.println("Jewels: " + j1 + "\nStones: " + s1 + "\nNumber of Stones that are Jewels: "
+                + bfNumStonesAreJewels(j1, s1));
 
         String j2 = "aksmlZ";
         String s2 = "ksakAKssaAzxcq";
-        System.out.println("Jewels: " + j2 + "\nStones: " + s2 + "\nNumber of Stones that are Jewels: " + eNumStonesAreJewels(j2, s2));
+        System.out.println("Jewels: " + j2 + "\nStones: " + s2 + "\nNumber of Stones that are Jewels: "
+                + eNumStonesAreJewels(j2, s2));
+
+        String j3 = "bBzx";
+        String s3 = "kjsdownebzBsabxBxxb";
+        System.out.println("Jewels: " + j3 + "\nStones: " + s3 + "\nNumber of Stones that are Jewels: "
+                + eNumStonesAreJewels(j3, s3));
     }
 }
