0ea7ef21f30d4633361f1c5a561a5d92d6a2c8d2
Sammy Samkough <sammysamkough@gmail.com>
2020-10-23 19:00:55 -0400

    Added sandbox file

---
+++ Playground.java
@@ -0,0 +1,14 @@
+public class Playground {
+    public static short countBits(int x) {
+        short numBits = 0;
+        while (x != 0) {
+            numBits += (x & 1);
+            x >>>= 1;
+        }
+        return numBits;
+    }
+
+    public static void main(String args[]) {
+        System.out.println(countBits(5));
+    }
+}
